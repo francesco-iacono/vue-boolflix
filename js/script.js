60352b65235c10752156f43c7cbc889707af5e82
francesco <f.iacono89@gmail.com>
2020-12-22 13:22:30 +0100

    quarto commit

--- js/script.js
+++ js/script.js
@@ -1,24 +1,3 @@
-// {
-//      "adult": false,
-//      "backdrop_path": "/fq3wyOs1RHyz2yfzsb4sck7aWRG.jpg",
-//      "genre_ids": [
-//          12,
-//          35,
-//          878,
-//          10751
-//      ],
-//      "id": 105,
-//      "original_language": "en",
-//      "original_title": "Back to the Future",
-//      "overview": "Marty McFly è stato catapultato per errore nel 1955, grazie alla macchina del tempo ideata dal suo amico scienziato Doc. Non avendo più \"carburante\" per poter tornare nel futuro si rivolge alla versione più giovane di Doc, che nonostante l'incredulità iniziale si farà in quattro per aiutarlo. Ma nel 1955 non è solo Doc ad essere più giovane, Marty infatti si imbatte casualmente nei suoi genitori, all'epoca teenager, ma l'incontro aggiungerà altri problemi.",
-//      "popularity": 48.943,
-//      "poster_path": "/AkmUoSHkxW9txpzZ52gCcWweEkE.jpg",
-//      "release_date": "1985-07-03",
-//      "title": "Ritorno al futuro",
-//      "video": false,
-//      "vote_average": 8.3,
-//      "vote_count": 14221
-//  }
 var root = new Vue({
   el: '#root',
   data: {
@@ -30,6 +9,24 @@ var root = new Vue({
   },
   methods: {
     searchMovie: function () {
+          this.movies = [],
+          axios
+          .get('https://api.themoviedb.org/3/search/movie',{
+            params: {
+            api_key:'9465910b329a81a6e089bd8b3ea2ac56',
+            query: this.searchString,
+            language: 'it-IT'
+          }})
+          .then( (result) => {
+            result.data.results.forEach((item, i) => {
+              let voteStar = Math.ceil(item.vote_average / 2);
+              let voteEmpty = 5 - voteStar;
+              const newObjMovie = {...item, voteStar: voteStar, voteEmpty: voteEmpty}
+              this.movies.push(newObjMovie);
+            }
+          );
+        }
+      );
       this.tvShows = [],
       axios
           .get('https:api.themoviedb.org/3/search/tv',{
@@ -46,73 +43,64 @@ var root = new Vue({
               this.tvShows.push(newObjTvShow);
             }
           );
-        }
-      );
-      this.movies = [],
-      axios
-          .get('https://api.themoviedb.org/3/search/movie',{
-            params: {
-            api_key:'9465910b329a81a6e089bd8b3ea2ac56',
-            query: this.searchString,
-            language: 'it-IT'
-          }})
-          .then( (result) => {
-            result.data.results.forEach((item, i) => {
-              let voteStar = Math.ceil(item.vote_average / 2);
-              let voteEmpty = 5 - voteStar;
-              const newObjMovie = {...item, voteStar: voteStar, voteEmpty: voteEmpty}
-              this.movies.push(newObjMovie);
-            }
-          );
-          console.log('Film',this.movies);
-          console.log('Serie',this.tvShows);
+          // unire gli array
           this.allCollections = [...this.movies,...this.tvShows];
-          console.log('Collections', this.allCollections);
+          // ordinare in base alla popolarità
+          this.allCollections.sort((a, b) => {
+          return parseInt(b.popularity) - parseInt(a.popularity);
+          });
+          this.allCollections = [...this.movies,...this.tvShows];
           this.searchString = '';
         }
       );
     }
   },
-  // mounted: function () {
-  //     axios
-  //       .get('https://api.themoviedb.org/3/search/movie?',{
-  //         params: {
-  //         api_key:'9465910b329a81a6e089bd8b3ea2ac56',
-  //         query: 'Ritorno',
-  //         language: 'it-IT'
-  //       }})
-  //       .then( (result) => {
-  //         this.movies = result.data.results;
-  //         result.data.results.forEach((item, i) => {
-  //           let voteStar = Math.ceil(item.vote_average / 2);
-  //           let voteEmpty = 5 - voteStar;
-  //           this.voteStar.push(voteStar);
-  //           this.voteEmpty.push(voteEmpty);
-  //         }
-  //       );
-  //     });
-  //     axios
-  //       .get('https:api.themoviedb.org/3/search/tv',{
-  //         params: {
-  //         api_key:'9465910b329a81a6e089bd8b3ea2ac56',
-  //         query: 'Ritorno',
-  //         language: 'it-IT'
-  //       }})
-  //       .then( (result) => {
-  //         this.tvShows = result.data.results;
-  //         this.allCollections = [...this.movies,...this.tvShows];
-  //         console.log(this.allCollections);
-  //         this.allCollections.forEach((item, i) => {
-  //           let voteStar = Math.ceil(item.vote_average / 2);
-  //           let voteEmpty = 5 - voteStar;
-  //           this.voteStar.push(voteStar);
-  //           this.voteEmpty.push(voteEmpty);
-  //           console.log(this.voteStar);
-  //         }
-  //       );
-  //     }
-  //   );
-  // }
+  mounted: function () {
+        axios
+        .get('https://api.themoviedb.org/3/search/movie',{
+          params: {
+          api_key:'9465910b329a81a6e089bd8b3ea2ac56',
+          query: 'suits',
+          language: 'it-IT'
+        }})
+        .then( (result) => {
+          result.data.results.forEach((item, i) => {
+            let voteStar = Math.ceil(item.vote_average / 2);
+            let voteEmpty = 5 - voteStar;
+            const newObjMovie = {...item, voteStar: voteStar, voteEmpty: voteEmpty}
+            this.movies.push(newObjMovie);
+          }
+        );
+      }
+    );
+    axios
+        .get('https:api.themoviedb.org/3/search/tv',{
+          params: {
+          api_key:'9465910b329a81a6e089bd8b3ea2ac56',
+          query: 'suits',
+          language: 'it-IT'
+        }})
+        .then( (result) => {
+          result.data.results.forEach((item, i) => {
+            let voteStar = Math.ceil(item.vote_average / 2);
+            let voteEmpty = 5 - voteStar;
+            const newObjTvShow = {...item, voteStar: voteStar, voteEmpty: voteEmpty}
+            this.tvShows.push(newObjTvShow);
+          }
+        );
+        console.log('Film',this.movies);
+        console.log('Serie',this.tvShows);
+        // unire gli array
+        this.allCollections = [...this.movies,...this.tvShows];
+        // ordinare in base alla popolarità
+        this.allCollections.sort((a, b) => {
+        return parseInt(b.popularity) - parseInt(a.popularity);
+        });
+        console.log('Collections', this.allCollections);
+        this.searchString = '';
+      }
+    );
+  }
 });
 
 
